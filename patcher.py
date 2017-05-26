d0ced8395d1987f7a03fcaa249dad12833e9268f
CairX <lazycairx@gmail.com>
2017-05-26 16:30:20 +0200

    Commands to install and uninstall based on the old restore command

--- patcher.py
+++ patcher.py
@@ -6,6 +6,7 @@ import sys
 VERSIONS_LOCATION = "versions"
 INSTALL_LOCATION = "install"
 VERSION_FILE = os.path.join(INSTALL_LOCATION, "version.txt")
+CHANGES_FILE = "changes.txt"
 
 
 def update():
@@ -19,12 +20,12 @@ def update():
 	print(versions[versions.index(current) + 1:])
 
 	for version in versions[versions.index(current) + 1:]:
-		patch(version)
+		patch(str(version))
 
 
 def patch(version):
-	version_path = os.path.join(VERSIONS_LOCATION, version)
-	version_changes = os.path.join(version_path, "changes.txt")
+	version_path = os.path.join(VERSIONS_LOCATION, str(version))
+	version_changes = os.path.join(version_path, CHANGES_FILE)
 
 	with open(version_changes) as changes:
 		for change in changes:
@@ -44,14 +45,38 @@ def patch(version):
 		file.write(version)
 
 
-def restore():
+def install(version=1):
+	version_path = os.path.join(VERSIONS_LOCATION, str(version))
+	shutil.copytree(version_path, INSTALL_LOCATION)
+	os.remove(os.path.join(INSTALL_LOCATION, CHANGES_FILE))
+
+	with open(VERSION_FILE, "w") as file:
+		file.write(str(version))
+
+
+def uninstall():
 	shutil.rmtree(INSTALL_LOCATION)
-	os.makedirs(INSTALL_LOCATION)
-	patch("1")
+
+
+def restore(version=1):
+	uninstall()
+	install(version)
 
 
 if __name__ == "__main__":
-	if len(sys.argv) == 1:
+	if len(sys.argv) > 1:
+		command =sys.argv[1]
+		if command == "install":
+			if len(sys.argv) >= 3:
+				install(sys.argv[2])
+			else:
+				install()
+		elif command == "uninstall":
+			uninstall()
+		elif command == "restore":
+			if len(sys.argv) >= 3:
+				restore(sys.argv[2])
+			else:
+				restore()
+	else:
 		update()
-	elif sys.argv[1] == "restore":
-		restore()
