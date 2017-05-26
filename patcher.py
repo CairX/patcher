eb88c822a025f636e665bf9f8fbe6451a5dc7c61
CairX <lazycairx@gmail.com>
2017-05-26 15:15:58 +0200

    Command to restore the install enviorment for easier testing

--- patcher.py
+++ patcher.py
@@ -1,10 +1,25 @@
 import os
 import shutil
+import sys
 
 
 VERSIONS_LOCATION = "versions"
 INSTALL_LOCATION = "install"
 VERSION_FILE = os.path.join(INSTALL_LOCATION, "version.txt")
+
+
+def update():
+	current = 0
+	with open(VERSION_FILE) as file:
+		current = file.readline().strip()
+	print(current)
+	versions = sorted(os.listdir("versions"))
+	print(versions)
+	print(versions.index(current))
+	print(versions[versions.index(current) + 1:])
+
+	for version in versions[versions.index(current) + 1:]:
+		patch(version)
 
 
 def patch(version):
@@ -29,15 +44,14 @@ def patch(version):
 		file.write(version)
 
 
-if __name__ == "__main__":
-	current = 0
-	with open(VERSION_FILE) as file:
-		current = file.readline().strip()
-	print(current)
-	versions = sorted(os.listdir("versions"))
-	print(versions)
-	print(versions.index(current))
-	print(versions[versions.index(current) + 1:])
+def restore():
+	shutil.rmtree(INSTALL_LOCATION)
+	os.makedirs(INSTALL_LOCATION)
+	patch("1")
 
-	for version in versions[versions.index(current) + 1:]:
-		patch(version)
+
+if __name__ == "__main__":
+	if len(sys.argv) == 1:
+		update()
+	elif sys.argv[1] == "restore":
+		restore()
